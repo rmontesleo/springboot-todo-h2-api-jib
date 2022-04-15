18953e5be2b61e7a2a3ccbed39b63415f4b7b5b5
Leopoldo Ramirez Montes <rmontesleo@gmail.com>
2022-04-15 00:49:21 -0500

    🚀 Initial commit

---
+++ DOCS/TEST_API.md
@@ -0,0 +1,9 @@
+# Testing the API
+
+initially
+
+http://localhost:8080/
+
+http://localhost:8080/swagger-ui/index.html
+
+---
---
+++ src/main/java/com/demo/todoapi/TodoApiContainerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiContainerApplication.class, args);
+	}
+
+}
